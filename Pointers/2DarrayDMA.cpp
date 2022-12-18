028d024200b4878bf39323036a7dac960c7ae151
dhananjay1401 <gaurdhananjay54@gmail.com>
2022-12-18 19:25:18 +0530

    commit

--- Pointers/2DarrayDMA.cpp
+++ Pointers/2DarrayDMA.cpp
@@ -4,26 +4,32 @@ using namespace std;
 
 int main(){
 
-int n;
-cin>>n;
-int **arr = new int *[n];
+int row;
+cin >> row;
 
-for(int i=0;i<n;i++){
-        arr[i] = new int [n];
+int col;
+cin >> col;
+
+int** arr = new int*[row];
+for(int i=0; i<row; i++){
+    arr[i] = new int[col];
 }
 
-
-for(int i=0; i<n; i++){
-    for(int j=0; j<n;j++){
-        cin>> arr[i][j];
+for(int i=0; i<row; i++){
+    for(int j=0; j<col; j++){
+        cin >> arr[i][j];
     }
 }
-
-for(int i=0; i<n; i++){
-    for(int j=0; i<n;j++){
-        cout<< arr[i][j];
-    }
+cout << endl;
+for(int i=0; i<row; i++){
+    for(int j=0; j<col; j++){
+        cout << arr[i][j] << " ";
+    }cout << endl;
 }
 
+for(int i = 0; i<row; i++) {
+    delete [] arr[i];
+}
 
+delete []arr;
 }
