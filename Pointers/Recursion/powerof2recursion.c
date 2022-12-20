c3d27f5c8964e79bf43900c52abac12b4bf6bf3b
dhananjay1401 <gaurdhananjay54@gmail.com>
2022-12-20 23:29:09 +0530

    commit

--- Pointers/Recursion/powerof2recursion.c
+++ Pointers/Recursion/powerof2recursion.c
@@ -1,1 +1,29 @@
+#include<iostream>
+using namespace std;
+double myPow(double a, int b) {
+        if(b==0){
+            return 1;
+        }
+        if(b==1){
+            return a;
+        }
 
+        double ans = myPow(a,b/2);
+
+        if(b%2 == 0){
+            return ans * ans;
+        }
+        else{
+            return a * ans * ans;
+        }
+
+
+    }
+int main(){
+int a,b;
+cin>>a;
+cin>>b;
+int ans = myPow(a,b);
+cout<<ans;
+
+}
