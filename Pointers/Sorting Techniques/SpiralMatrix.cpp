b594caaefa0ffe6d3699a13cdd41341276e60c7d
dhananjay1401 <gaurdhananjay54@gmail.com>
2022-12-26 15:58:31 +0530

    commit

--- Pointers/Sorting Techniques/SpiralMatrix.cpp
+++ Pointers/Sorting Techniques/SpiralMatrix.cpp
@@ -2,5 +2,12 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
 
+     vector<int> ans;
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int count = 0;
+        int total = row*col;
+
 
 };
