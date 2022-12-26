b6278e4742db052a946b053a067b7d43ef1e13fa
dhananjay1401 <gaurdhananjay54@gmail.com>
2022-12-26 15:58:52 +0530

    commit

--- Pointers/Sorting Techniques/SpiralMatrix.cpp
+++ Pointers/Sorting Techniques/SpiralMatrix.cpp
@@ -9,5 +9,19 @@ public:
         int count = 0;
         int total = row*col;
 
+        //Index Initalize
+        int startingRow = 0;
+        int startingCol = 0;
+        int endingRow = row-1;
+        int endingCol = col-1;
+
+        while(count<total){
+            for(int index = startingCol; count<total &&  index<=endingCol; index++) {
+                ans.push_back(matrix[startingRow][index]);
+                count++;
+            }
+            startingRow++;
+
+
 
 };
