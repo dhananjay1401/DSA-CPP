40ae032155629823c364cc5f25451abf184b3304
dhananjay1401 <gaurdhananjay54@gmail.com>
2022-12-27 22:47:01 +0530

    commit

--- Pointers/Sorting Techniques/SpiralMatrix.cpp
+++ Pointers/Sorting Techniques/SpiralMatrix.cpp
@@ -1,9 +1,8 @@
 
      for(int i= endingRow; count<total && i>=startingRow; i--){
-                ans.push_back(matrix[i][startingCol]);
-            count++;
 
 
+            }
  startingCol++;
 
 
