96fb101fd6373f8311185bc588894e952eeec9ea
dhananjay1401 <gaurdhananjay54@gmail.com>
2022-12-28 16:44:23 +0530

    commit

--- Pointers/LinkedList/linklistclass.cpp
+++ Pointers/LinkedList/linklistclass.cpp
@@ -20,7 +20,7 @@ head = temp;
 int print(Node* &head){
 Node* temp =head;
 while(temp !=NULL){
-        cout<<temp->data<<" "<<endl;
+        cout<<temp->data<<" ";
         temp = temp -> next;
 }
 }
@@ -28,19 +28,8 @@ while(temp !=NULL){
 int main(){
 Node* node1 = new Node(10);
 Node* head = node1;
-insertAtHead(head,20);
-print(head);
-insertAtHead(head,20);
-insertAtHead(head,40);
 insertAtHead(head,30);
-insertAtHead(head,10);
-insertAtHead(head,230);
-insertAtHead(head,439530);
-insertAtHead(head,2320);
-insertAtHead(head,120);
-insertAtHead(head,3240);
-insertAtHead(head,2340);
-insertAtHead(head,2340);
+insertAtHead(head,40);
 print(head);
 
 return 0;
