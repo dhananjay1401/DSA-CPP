f45d1396bf33a5e16aea0bfb01a7491dbf432d14
dhananjay1401 <gaurdhananjay54@gmail.com>
2022-12-28 23:11:49 +0530

    commit

--- Pointers/LinkedList/doubleLinklist.cpp
+++ Pointers/LinkedList/doubleLinklist.cpp
@@ -14,7 +14,12 @@ public:
     }
 };
 
-void insertAtHead(Node* &head)
+void insertAtHead(Node* &head, int d){
+Node* temp = new Node(d);
+temp -> next = head;
+head -> prev = temp;
+head = temp;
+}
 
 void print(Node* &head){
 Node* temp = head;
@@ -38,6 +43,7 @@ int main(){
 
 Node* node1 = new Node(10);
 Node* head = node1;
+insertAtHead(head,40);
 print(head);
 getLength(head);
 return 0;
