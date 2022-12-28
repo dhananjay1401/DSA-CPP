8dc30b10bd3fd6f29ffeb36e75abb96d2abd98bb
dhananjay1401 <gaurdhananjay54@gmail.com>
2022-12-28 16:36:49 +0530

    commit

--- Pointers/LinkedList/linklistclass.cpp
+++ Pointers/LinkedList/linklistclass.cpp
@@ -3,23 +3,22 @@ using namespace std;
 
 class Node{
 
+public:
+int data;
+Node* next;
 
- public:
-     int data;
-     Node* next;
+Node(int data){
+this -> data = data;
+this -> next = NULL;
+}
 
-     Node(int data){
-     this -> data = data;
-     this -> next = NULL;
-
-     }
 };
 
 int main(){
+Node* node1 = new Node(10);
+cout<<node1->data<<endl;
 
-Node* node1 =new Node(100);
-
-cout<<node1 -> data<<endl;
-cout<<node1 -> next<<endl;
-
+return 0;
 }
+
+
