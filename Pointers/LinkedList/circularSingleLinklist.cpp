1d55f81de7539da3e2e08fc98a67ba7c019001a5
dhananjay1401 <gaurdhananjay54@gmail.com>
2022-12-29 16:37:48 +0530

    commit

---
+++ Pointers/LinkedList/circularSingleLinklist.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+int data;
+Node* next;
+Node(int data){
+this -> data = data;
+this -> next = NULL;
+}
+  ~Node() {
+        int value = this -> data;
+        //memory free
+        if(this->next != NULL) {
+            delete next;
+            this->next = NULL;
+        }
+        cout << " memory is free for node with data " << value << endl;
+    }
+};
+
+
+void insertNode(Node* &tail, int element, int d){
+ if(tail == NULL){
+    Node* newNode = new Node(d);
+    tail = newNode;
+    newNode -> next = newNode;
+}else{
+Node* curr = tail;
+while(curr -> data != element){
+    curr = curr -> next;
+}
+Node* temp = new Node(d);
+temp -> next = curr -> next;
+curr -> next = temp;
+}
+}
+
+void print(Node*tail){
+    Node* temp = tail;
+do{
+    cout<<tail->data<<" ";
+    tail = tail ->next;
+}while(tail!=temp);
+cout<<endl;
+}
+
+
+int main(){
+Node* tail = NULL;
+insertNode(tail,5,3);
+print(tail);
+
+insertNode(tail,3,10);
+print(tail);
+
+insertNode(tail,3,13);
+print(tail);
+
+insertNode(tail,13,5);
+print(tail);
+return 0;
+}
+
+
+
--- Pointers/LinkedList/doubleLinklist.cpp
+++ Pointers/LinkedList/doubleLinklist.cpp
@@ -6,19 +6,101 @@ public:
     int data;
     Node* next;
     Node* prev;
-
     Node(int d){
     this -> data = d;
     this -> next = NULL;
     this -> prev = NULL;
     }
+
+     ~Node() {
+        int value = this -> data;
+        //memory free
+        if(this->next != NULL) {
+            delete next;
+            this->next = NULL;
+        }
+        cout << " memory is free for node with data " << value << endl;
+    }
+
 };
 
-void insertAtHead(Node* &head, int d){
-Node* temp = new Node(d);
+
+void insertAtHead(Node* &tail, Node* &head, int d){
+if(head==NULL){
+    Node* temp = new Node(d);
+    head = temp;
+    tail = temp;
+}else{
+    Node* temp = new Node(d);
 temp -> next = head;
 head -> prev = temp;
 head = temp;
+}
+
+
+}
+
+void insertAtTail(Node* &tail,Node* &head, int d){
+    if(tail==NULL){
+    Node* temp = new Node(d);
+    tail = temp;
+    head = temp;
+}else{
+Node* temp = new Node(d);
+temp -> prev = tail;
+tail -> next = temp;
+tail = temp;
+}
+}
+
+void insertAtPosition(Node* &tail,Node* &head, int position, int d){
+if(position==1){
+    insertAtHead(tail,head,d);
+    return ;
+}
+
+Node* temp = head;
+int cnt = 1;
+while(cnt<position-1){
+    temp = temp -> next;
+    cnt++;
+}
+
+if(temp->next == NULL){
+    insertAtTail(tail,head,d);
+    return ;
+}
+Node* nodeToInsert = new Node(d);
+
+nodeToInsert -> next = temp -> next;
+temp -> next -> prev = nodeToInsert;
+temp -> next = nodeToInsert;
+nodeToInsert -> prev = temp;
+
+}
+void deleteNode(Node* &head, int position){
+
+if(position ==1){
+    Node* temp =head;
+    temp -> next -> prev = NULL;
+    head = temp -> next;
+    temp -> next =  NULL;
+    delete temp;
+}else{
+Node* curr =head;
+Node* pre = NULL;
+int cnt = 1;
+while(cnt<position){
+    pre = curr;
+    curr = curr -> next;
+    cnt++;
+}
+curr -> prev = NULL;
+pre -> next = curr-> next;
+curr -> next -> prev = pre;
+curr -> next = NULL;
+delete curr;
+}
 }
 
 void print(Node* &head){
@@ -41,9 +123,14 @@ return len;
 
 int main(){
 
-Node* node1 = new Node(10);
-Node* head = node1;
-insertAtHead(head,40);
+Node* head = NULL;
+Node* tail = NULL;
+insertAtHead(tail,head,40);
+insertAtTail(tail,head,20);
+insertAtHead(tail,head,90);
+insertAtPosition(tail,head,4,9);
+print(head);
+deleteNode(head , 2);
 print(head);
 getLength(head);
 return 0;
