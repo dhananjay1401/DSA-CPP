2d56d16eed7eef2d0eb01a58e1eb173bf072da28
dhananjay1401 <gaurdhananjay54@gmail.com>
2022-12-30 19:42:41 +0530

    commit

--- Pointers/LinkedList/Questions/ReverseK.cpp
+++ Pointers/LinkedList/Questions/ReverseK.cpp
@@ -21,16 +21,7 @@ if(head==NULL){
     return head;
 }
 
-    Node* forward = NULL;
-    Node* curr = head;
-    Node* prev = NULL;
-    int count = 0;
-    while(curr!= NULL && count<k){
-        forward = curr -> next;
-        curr -> next = prev;
-        prev = curr;
-        curr = forward;
-        count++;
+
     }
     if(forward!=NULL){
         head -> next = kReverse(forward,k);
