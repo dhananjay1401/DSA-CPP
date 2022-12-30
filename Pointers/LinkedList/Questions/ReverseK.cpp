7131d2430e10396d479de47cbbf76da4acfee815
dhananjay1401 <gaurdhananjay54@gmail.com>
2022-12-30 19:41:24 +0530

    commit

---
+++ Pointers/LinkedList/Questions/ReverseK.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+/****************************************************************
+    Following is the Linked List node structure
+
+    class Node
+    {
+    public:
+        int data;
+        Node *next;
+        Node(int data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+
+Node* kReverse(Node* head, int k) {
+if(head==NULL){
+    return head;
+}
+    Node* forward = NULL;
+    Node* curr = head;
+    Node* prev = NULL;
+    int count = 0;
+    while(curr!= NULL && count<k){
+        forward = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = forward;
+        count++;
+    }
+    if(forward!=NULL){
+        head -> next = kReverse(forward,k);
+    }
+    return prev;
+}
