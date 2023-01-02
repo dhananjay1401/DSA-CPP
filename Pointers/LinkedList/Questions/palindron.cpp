4a1879f81d9f98ce8d314e637678f60d8e2bfd7a
dhananjay1401 <gaurdhananjay54@gmail.com>
2023-01-03 00:37:57 +0530

    commit

--- Pointers/LinkedList/Questions/palindron.cpp
+++ Pointers/LinkedList/Questions/palindron.cpp
@@ -8,4 +8,35 @@ class Solution{
 
       while(fast!=NULL){
         fast = fast -> next;
+        if(fast!=NULL){
+        fast = fast -> next;
+        }
+        slow = slow -> next;
+      }
+      return slow;
+      }
 
+      Node* reverse(Node* head){
+      Node* curr = head;
+      Node* prev = NULL;
+      Node* forward =NULL;
+
+      while(curr!=NULL){
+        forward = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = forward;
+      }
+      return prev;
+      }
+  public:
+    //Function to check whether the list is palindrome.
+    bool isPalindrome(Node *head)
+    {
+    Node* mid = getMid(head);
+    Node* temp = mid ->next;
+    mid -> next = reverse(temp);
+    Node* head1 = head;
+    Node* head2 = mid->next;
+
+    while(head2=NULL){
