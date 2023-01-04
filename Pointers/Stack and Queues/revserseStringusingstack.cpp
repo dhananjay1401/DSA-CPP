19f9d0f0f3774818a93e8acb5a9591adf7412ab0
dhananjay1401 <gaurdhananjay54@gmail.com>
2023-01-04 21:38:23 +0530

    commit

---
+++ Pointers/Stack and Queues/revserseStringusingstack.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+string str = "babbar";
+stack<char> s;
+
+for(int i=0; i<str.length();i++){
+    char ch = str[i];
+    s.push(ch);
+}
+
+
+
+
+
+
+}
