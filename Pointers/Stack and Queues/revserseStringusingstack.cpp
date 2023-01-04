ad7142c6aa9d2f64cd12b5f8ffa572aafb9805f5
dhananjay1401 <gaurdhananjay54@gmail.com>
2023-01-04 21:46:06 +0530

    commit

--- Pointers/Stack and Queues/revserseStringusingstack.cpp
+++ Pointers/Stack and Queues/revserseStringusingstack.cpp
@@ -11,9 +11,13 @@ for(int i=0; i<str.length();i++){
     s.push(ch);
 }
 
+     string ans = "";
+     while(!s.empty()){
+         char ch = s.top();
+         ans.push_back(ch);
+         s.pop();
+     }
+     cout<<ans<<endl;
 
-
-
-
-
+return 0;
 }
