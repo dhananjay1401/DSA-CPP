addad86e3c46e90e23b296891476652e7a06ce09
dhananjay1401 <gaurdhananjay54@gmail.com>
2023-01-07 22:21:07 +0530

    commit

--- Pointers/LinkedList/Questions/NextSmallerElement.cpp
+++ Pointers/LinkedList/Questions/NextSmallerElement.cpp
@@ -8,3 +8,16 @@ vector<int> nextSmallerElement(vector<int> &arr, int n)
  s.push(-1);
    vector <int> ans;
 
+   for(int i=n-1; i>=0; i--)
+    {
+        int curr = arr[i];
+        while(s.top()>=curr){
+            s.pop();
+        }
+        ans[i] = s.top();
+        s.push(curr);
+    }
+
+    return ans
+
+}
