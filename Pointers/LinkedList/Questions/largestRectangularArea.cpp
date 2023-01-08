42358c27c2aa2c929678ac594bd10a88c09e3e1e
dhananjay1401 <gaurdhananjay54@gmail.com>
2023-01-08 15:56:47 +0530

    commit

--- Pointers/LinkedList/Questions/largestRectangularArea.cpp
+++ Pointers/LinkedList/Questions/largestRectangularArea.cpp
@@ -1,1 +1,10 @@
 
+int largestRectangleArea(vector<int>& heights) {
+int n = heights.size();
+
+vector<int> next(n);
+next = nextSmallerElement(heights, n);
+
+vector<int> prev(n);
+prev = prevSmallerElement(heights, n);
+int area
