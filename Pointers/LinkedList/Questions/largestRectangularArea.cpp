5ca9e930681549d2ae5352b5e12c19696ef00903
dhananjay1401 <gaurdhananjay54@gmail.com>
2023-01-08 15:57:01 +0530

    commit

--- Pointers/LinkedList/Questions/largestRectangularArea.cpp
+++ Pointers/LinkedList/Questions/largestRectangularArea.cpp
@@ -7,4 +7,17 @@ next = nextSmallerElement(heights, n);
 
 vector<int> prev(n);
 prev = prevSmallerElement(heights, n);
-int area
+int area = INT_MIN;
+
+for(int i = 0; i<n;i++){
+    int l = heights[i];
+    int b = next[i] - prev[i] - 1;
+
+    if(next[i] == -1){
+        next[i] = n;
+    }
+    int newArea = l*b;
+    area = max(area, newArea);
+}
+return area;
+}
