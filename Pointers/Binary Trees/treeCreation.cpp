03e691f7da80e95bfd4c403a283be3856b861492
dhananjay1401 <gaurdhananjay54@gmail.com>
2023-01-19 00:32:49 +0530

    push

---
+++ Pointers/Binary Trees/treeCreation.cpp
@@ -0,0 +1,38 @@
+#include iostream;
+using namespace std;
+
+class node{
+public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int d){
+    this -> data = d;
+    this -> left = NULL;
+    this -> right = NULL;
+    }
+};
+
+
+node* buildTree(node* root){
+cout<<"Enter Data"<<endl;
+int data;
+cin>>data<<endl;
+root = new node(data);
+if(data == -1){
+    return NULL;
+}
+cout<<"Enter data for inserting in left for"<<data<<endl;
+root -> left = buildTree(root -> left);
+cout<<"Enter data for inserting in right for"<<data<<endl;
+root -> right = buildTree(root -> right);
+
+return root;
+}
+
+int main(){
+    node* root = NULL;
+    root = buildTree(root);
+    return 0;
+}
